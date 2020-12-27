872b30f369da58ac73f8fb7e43bf675e9fdc1a53
kimura <pankata79.th@gmail.com>
2020-12-27 19:57:35 +0900

    yy

--- src/main/java/com/example/demo/controller/ErrorController.java
+++ src/main/java/com/example/demo/controller/ErrorController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class ErrorController {
 
 	@ExceptionHandler(NullPointerException.class)
@@ -19,5 +21,10 @@ public class ErrorController {
 	@RequestMapping(value = "/error",method = RequestMethod.GET)
 	public String errorpage() throws Exception{
 		throw new Exception();
+	}
+	@PostMapping("/login")
+	public String error() {
+		return "redirect:/login";
+		
 	}
 }
