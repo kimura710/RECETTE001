5403023659d99c858028a3a318b71dcae634cb82
kimura <pankata79.th@gmail.com>
2021-01-01 20:12:09 +0900

    test

---
+++ src/test/java/com/example/demo/controllerTest/EditControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllerTest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.EditController;
+import com.example.demo.service.MenuService;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EditControllerTest {
+	
+	private MockMvc sut;
+	
+	@MockBean
+	private MenuService service;
+	
+	@Autowired
+	private EditController target;
+	
+	@Before
+
+	
+}
